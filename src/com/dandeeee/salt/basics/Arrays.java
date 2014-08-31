4993c1f6e6e1269d70db15f16b369b7b837d07f3
Daniel <dan.dedkov@gmail.com>
2014-08-31 10:10:46 +0300

    Demo of basic working with arrays
    Memory, links, copy

---
+++ src/com/dandeeee/salt/basics/Arrays.java
@@ -0,0 +1,34 @@
+package com.dandeeee.salt.basics;
+
+
+/**
+ * Basic array understanding
+ *
+ * @author dandedkov.com
+* */
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        String[] s = {"a", "b", "c"};
+		s = new String[] {"a", "b", "c"};
+		s = new String[3];
+		System.out.println(s[0] + " " + s.length);
+
+		int[] a = {1, 2, 3};
+		int[] b = new int[a.length + 1];
+		System.arraycopy(a, 0, b, 0, a.length);
+		System.out.println(java.util.Arrays.toString(b));
+
+		for (int element : b) {
+			System.out.print(element + " ");
+		}
+		System.out.println();
+
+		System.out.println(a instanceof Object);
+		System.out.println(s instanceof Object);
+		System.out.println(s instanceof String[]);
+		System.out.println(s instanceof Object[]);
+	}
+
+}
